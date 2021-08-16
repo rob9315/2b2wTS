c520f69ee52c66e226d87fc392d7889fe5e88311
rob9315 <dev.robk@gmail.com>
2021-08-16 22:05:11 +0200

    remove undwanted copies

--- build/src/discord.js
+++
@@ -1,7 +0,0 @@
-import {escapeRegExp} from "./util.js";
-export async function onDiscordMessage(msg) {
-  if (!!this.discord && !!this.options.discord?.commands && (!this.options.discord?.commands.allowedIds || this.options.discord?.commands.allowedIds?.includes(msg.author.id)) && msg.author.id !== this.discord.user?.id && !!msg.content.match(`^${escapeRegExp(this.options.discord?.commands?.prefix ?? "")}`))
-    try {
-    } catch {
-    }
-}
--- build/svelte/index.js
+++
@@ -1,3 +0,0 @@
-import Index from "./index.svelte";
-const index = new Index({target: document.body});
-export default index;
